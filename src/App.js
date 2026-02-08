63155075d20bfbf9b6e925241458ea4b2a72e72f
Saksham Saraogi <154125803+SakshamSaraogi@users.noreply.github.com>
2026-02-08 05:14:44 +0000

    Refresh Page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  useNavigate,
+} from "react-router-dom";
+
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import LoadingScreen from "./pages/LoadingScreen.jsx";
 import ErrorScreen from "./pages/ErrorScreen.jsx";
 import PhoneLock from "./pages/PhoneLock.jsx";
@@ -12,24 +20,43 @@ import HeartLoading from "./pages/HeartLoading.jsx";
 import ConnectionEstablished from "./pages/ConnectionEstablished.jsx";
 import LoveLetter from "./pages/LoveLetter.jsx";
 
+/* 🔒 FORCE RESET ON REFRESH */
+function ResetOnRefresh({ children }) {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const isReload =
+      performance.getEntriesByType("navigation")[0]?.type === "reload";
+
+    if (isReload && location.pathname !== "/") {
+      navigate("/", { replace: true });
+    }
+  }, [location, navigate]);
+
+  return children;
+}
+
 function App() {
   return (
-    <div className="App">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoadingScreen />} />
-          <Route path="/error" element={<ErrorScreen />} />
-          <Route path="/phonelock" element={<PhoneLock />} />
-          <Route path="/recovery" element={<RecoveryProtocol />} />
-          <Route path="/saving" element={<SavingScreen />} />
-          <Route path="/tictactoe" element={<TicTacToe />} />
-          <Route path="/valentine" element={<ValentineQuestion />} />
-          <Route path="/heart-loading" element={<HeartLoading />} />
-          <Route path="/established" element={<ConnectionEstablished />} />
-          <Route path="/letter" element={<LoveLetter />} />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <ResetOnRefresh>
+        <div className="App">
+          <Routes>
+            <Route path="/" element={<LoadingScreen />} />
+            <Route path="/error" element={<ErrorScreen />} />
+            <Route path="/phonelock" element={<PhoneLock />} />
+            <Route path="/recovery" element={<RecoveryProtocol />} />
+            <Route path="/saving" element={<SavingScreen />} />
+            <Route path="/tictactoe" element={<TicTacToe />} />
+            <Route path="/valentine" element={<ValentineQuestion />} />
+            <Route path="/heart-loading" element={<HeartLoading />} />
+            <Route path="/established" element={<ConnectionEstablished />} />
+            <Route path="/letter" element={<LoveLetter />} />
+          </Routes>
+        </div>
+      </ResetOnRefresh>
+    </BrowserRouter>
   );
 }
 
